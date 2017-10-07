78aa84bfe8b89f06ecff9e3418da91f0126ca6e5
Rini Wilson <rini1000.rw@gmail.com>
2017-10-07 04:27:12 -0700

    the auto text command

---
+++ Myfirstproject/src/com/rini1000/autotype_text_demo.java
@@ -0,0 +1,72 @@
+package com.rini1000;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+public class autotype_text_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Runtime.getRuntime().exec("notepad.exe");
+			Thread.sleep(2000);
+			Robot r = new Robot();
+			r.keyPress(KeyEvent.VK_T);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_H);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_S);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_S);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_R);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_N);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_W);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_I);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_L);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_S);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_O);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_N);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_C);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// extra comments added
+	}
+
+}
