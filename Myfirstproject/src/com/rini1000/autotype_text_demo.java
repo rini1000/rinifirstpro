97106fcd8fc25159a5144be891121c712c0957b7
Rini Wilson <rini1000.rw@gmail.com>
2017-10-08 10:44:24 -0700

    i just add space

--- Myfirstproject/src/com/rini1000/autotype_text_demo.java
+++ Myfirstproject/src/com/rini1000/autotype_text_demo.java
@@ -122,7 +122,8 @@ public class autotype_text_demo {
 			r.keyPress(KeyEvent.VK_SPACE);
 			Thread.sleep(500);
 			r.keyPress(KeyEvent.VK_C);
-			
+			Thread.sleep(500);
+			r.keyPress(KeyEvent.VK_SPACE);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
